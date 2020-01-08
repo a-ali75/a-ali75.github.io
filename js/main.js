b2a0648d73a5d4197327361433a9ec7482efdf1f
Arju ALI <arjuali75@gmail.com>
2020-01-09 01:24:42 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,5 +4,10 @@ $(document).ready(function()
     {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
+        $('.box-img').toggleClass('close');
+        $('.para').toggleClass('close');
+        $('.title').toggleClass('close');
+        $('.underline_ad').toggleClass('close');
+        $('.box').toggleClass('resize');
     });
 });
